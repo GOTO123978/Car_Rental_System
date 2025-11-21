9d2c8dead90a6bae1aa9b1de57487894207765c1
GOTO123978 <a0981640016@gmail.com>
2025-11-21 15:29:45 +0800

    add Branch final

---
+++ src/main/java/com/demo/Service/BranchService.java
@@ -0,0 +1,22 @@
+package com.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.model.Branch;
+import com.demo.repository.BranchDao;
+
+@Service
+public class BranchService {
+	@Autowired
+	BranchDao repo;
+
+	public List<Branch> getBranchesByRegion(String region) {
+		if (region == null || region.trim().isEmpty() || region.equals("All")) {
+			return repo.findAll();
+		}
+		return repo.findByRegion(region);
+	}
+}
--- src/main/java/com/demo/model/Branch.java
+++ src/main/java/com/demo/model/Branch.java
@@ -1,28 +1,51 @@
 package com.demo.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.NamedQuery;
 import lombok.Data;
 
+/**
+ * The persistent class for the branch database table.
+ * 
+ */
 @Data
 @Entity
-@Table(name = "branch")
-public class Branch {
+@NamedQuery(name = "Branch.findAll", query = "SELECT b FROM Branch b")
+public class Branch implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@Column(name = "branch_name", length = 50)
-	private String branchName;
+	private String address;
 
-	@Column(length = 50)
+	@Column(name = "branch_id")
+	private String branchId;
+
 	private String city;
 
-	@Column(length = 100)
-	private String address;
+	@Column(name = "map_thumbnail_url")
+	private String mapThumbnailUrl;
+
+	@Column(name = "name_en")
+	private String nameEn;
+
+	@Column(name = "name_zh")
+	private String nameZh;
+
+	@Column(name = "opening_hours")
+	private String openingHours;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+
+	private String region;
+
 }
